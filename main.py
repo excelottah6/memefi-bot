031df6440f3a5c80406336a37dbb04f0195c4c59
EXCEL <119881740+excelottah6@users.noreply.github.com>
2024-09-28 15:26:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import sys
+import ssl
+import urllib3
 
 from utils.core import main
 from utils.helpers import print_banner, log, mrh
+
+
+ssl._create_default_https_context = ssl._create_unverified_context
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 if __name__ == "__main__":
     while True:
